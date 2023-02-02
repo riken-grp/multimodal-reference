16403f5b06fe605ea5253ae06ca33df58ebf03b1
nobu-g <ueda-n127@hotmail.co.jp>
2023-02-02 17:30:43 +0900

    update run_separate_model.sh

--- run_separate_model.sh
+++ run_separate_model.sh
@@ -2,10 +2,14 @@
 
 set -euo pipefail
 
-exp_name=mdetr_mixed_1e_flickr_1e
-scenario_ids=$(ls data/dataset)
+exp_name=mdetr_mixed_2e_flickr_2e
+scenario_ids=$(cat full_annotated.txt)
 
 for scenario_id in $scenario_ids; do
   echo "Running scenario $scenario_id"
+  if [[ -f "result/${exp_name}/${scenario_id}.json" ]]; then
+    echo "Skip"
+    continue
+  fi
   poetry run python src/prediction_writer.py -cn server dataset_dir="data/dataset/${scenario_id}" gold_knp_file="data/knp/${scenario_id}.knp" prediction_dir="result/${exp_name}"
 done
