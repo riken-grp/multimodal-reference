e8ee0ce54f014a2a24e6d531b79c0b0d64cd574b
nobu-g <ueda-n127@hotmail.co.jp>
2023-11-15 22:30:54 +0900

    add GLIPPhraseGrounding task

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,5 +1,6 @@
 from tasks.cohesion import CohesionAnalysis
+from tasks.glip import GLIPPhraseGrounding
 from tasks.mdetr import MDETRPhraseGrounding
 from tasks.mot import MultipleObjectTracking
 
-__all__ = ["CohesionAnalysis", "MDETRPhraseGrounding", "MultipleObjectTracking"]
+__all__ = ["CohesionAnalysis", "MDETRPhraseGrounding", "GLIPPhraseGrounding", "MultipleObjectTracking"]
