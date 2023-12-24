31debe8a33e0ae1957186c026d047c2b3f82d5a2
nobu-g <ueda-n127@hotmail.co.jp>
2023-12-24 23:10:13 +0900

    implement worker management for mot task

--- src/tasks/mot.py
+++ src/tasks/mot.py
@@ -1,3 +1,5 @@
+import os
+import socket
 import subprocess
 from pathlib import Path
 from typing import Annotated
@@ -6,14 +8,19 @@ import luigi
 from omegaconf import DictConfig
 
 from tasks.detic_detection import DeticObjectDetection
+from tasks.util import FileBasedResourceManagerMixin
 
 
-class MultipleObjectTracking(luigi.Task):
+class MultipleObjectTracking(luigi.Task, FileBasedResourceManagerMixin[int]):
     scenario_id: Annotated[str, luigi.Parameter()] = luigi.Parameter()
     cfg: Annotated[DictConfig, luigi.Parameter()] = luigi.Parameter()
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        available_gpus = [int(gpu_id) for gpu_id in os.environ.get("AVAILABLE_GPUS", "0").split(",")]
+        super(luigi.Task, self).__init__(
+            available_gpus, Path("shared_state.json"), state_prefix=f"{socket.gethostname()}_gpu"
+        )
         Path(self.cfg.prediction_dir).mkdir(exist_ok=True, parents=True)
 
     def requires(self) -> luigi.Task:
@@ -40,13 +47,23 @@ class MultipleObjectTracking(luigi.Task):
     def run(self) -> None:
         cfg = self.cfg
         input_video_file = Path(cfg.recording_dir) / self.scenario_id / "fp_video.mp4"
-        subprocess.run(
-            [
-                cfg.python,
-                f"{cfg.project_root}/src/mot_strong_sort.py",
-                input_video_file,
-                f"--detic-dump={self.input().path}",
-                f"--output-json={self.output().path}",
-            ],
-            check=True,
-        )
+
+        if (gpu_id := self.acquire_resource()) is None:
+            raise RuntimeError("No available GPU.")
+        try:
+            env = os.environ.copy()
+            env["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
+
+            subprocess.run(
+                [
+                    cfg.python,
+                    f"{cfg.project_root}/src/mot_strong_sort.py",
+                    input_video_file,
+                    f"--detic-dump={self.input().path}",
+                    f"--output-json={self.output().path}",
+                ],
+                check=True,
+                env=env,
+            )
+        finally:
+            self.release_resource(gpu_id)
