4ce9a9a38bacb4aa009633efe3d67743a3cf7f24
nobu-g <ueda-n127@hotmail.co.jp>
2024-01-03 23:27:10 +0900

    fix gen_relax_table.py

--- scripts/gen_relax_table.py
+++ scripts/gen_relax_table.py
@@ -38,15 +38,15 @@ class RatioMetric:
         return self.positive / self.total
 
 
-def gen_relax_comparison_profile(phrase_grounding_model: str, model_name: str) -> list[ExpConfig]:
+def gen_relax_comparison_profile(phrase_grounding_model: str, model_name: str, mot_name: str) -> list[ExpConfig]:
     configs = []
     relax_modes: list[tuple[Optional[str], Optional[str]]] = [
         (None, None),
         ("pred", None),
         ("gold", None),
-        (None, "default-detic_th0.3-max"),
+        (None, f"default-{mot_name}-max"),
         (None, "gold-max"),
-        ("pred", "default-detic_th0.3-max"),
+        ("pred", f"default-{mot_name}-max"),
         ("gold", "gold-max"),
     ]
     for coref_relax_mode, mot_relax_mode in relax_modes:
@@ -63,35 +63,36 @@ def gen_relax_comparison_profile(phrase_grounding_model: str, model_name: str) -
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("glip_name", default="ft2_deberta_b32_new_mixed_1e", help="Config name for GLIP model.")
+    parser.add_argument("mot_name", default="detic_th0.3", help="Config name for GLIP model.")
     # https://github.com/gregbanks/python-tabulate#table-format
-    parser.add_argument("glip_name", default="ft2_deberta_mixed_0.5e", help="Config name for GLIP model.")
-    parser.add_argument("--table-format", default="github", help="Table format.")
+    parser.add_argument("--table-format", "--table-fmt", default="github", help="Table format.")
     parser.add_argument(
         "--recall-topk", "--topk", type=int, nargs="*", default=[1, 5, 10], help="For calculating Recall@k."
     )
     parser.add_argument(
         "--id-file", type=Path, nargs="+", default=[Path("data/id/test.id")], help="Paths to scenario id file"
     )
+    parser.add_argument("--include-precision", action="store_true", help="Include precision in the table.")
     args = parser.parse_args()
     scenario_ids: list[str] = sum((path.read_text().splitlines() for path in args.id_file), [])
-    exp_configs = gen_relax_comparison_profile("glip", args.glip_name)
+    exp_configs = gen_relax_comparison_profile("glip", args.glip_name, args.mot_name)
     data: dict[ExpConfig, list[str]] = {config: [] for config in exp_configs}
     for exp_config in exp_configs:
         exp_name = exp_config.get_name()
         precisions = []
         command = (
             f"{sys.executable} src/evaluation.py"
-            f" -d data/dataset"
-            f" -k data/knp"
-            f" -a data/image_text_annotation"
+            " -d data/dataset"
+            " -k data/knp"
+            " -a data/image_text_annotation"
             f" -p result/mmref/{exp_name}"
-            f" --prediction-knp-dir result/cohesion"
-            # f" --prediction-mot-dir result/mot/default-th0.3"
+            " --prediction-knp-dir result/cohesion"
             f" --scenario-ids {' '.join(scenario_ids)}"
             f" --recall-topk {' '.join(map(str, args.recall_topk))}"
-            f" --th 0.0"
-            f" --eval-modes rel"
-            f" --format csv"
+            " --th 0.0"
+            " --eval-modes rel"
+            " --format csv"
         )
         try:
             output = subprocess.run(command.split(), capture_output=True, check=True, text=True)
@@ -100,16 +101,20 @@ def main():
             raise e
         rel_metric_table = pl.read_csv(io.StringIO(output.stdout))
         df_rel = rel_metric_table.filter(pl.col("rel_type") == "=")
-        for recall_top_k in (1, 5, 10):
+        for recall_top_k in args.recall_topk:
             recall = RatioMetric(df_rel[f"recall_pos@{recall_top_k}"][0], df_rel["recall_total"][0])
             data[exp_config].append(f"{recall.ratio:.3f} ({recall.positive})")
-        precisions.append(RatioMetric(df_rel["precision_pos"][0], df_rel["precision_total"][0]))
-        data[exp_config].append(f"{precisions[0].ratio:.3f} ({precisions[0].positive} / {precisions[0].total})")
+        if args.include_precision:
+            precisions.append(RatioMetric(df_rel["precision_pos"][0], df_rel["precision_total"][0]))
+            data[exp_config].append(f"{precisions[0].ratio:.3f} ({precisions[0].positive} / {precisions[0].total})")
 
+    headers = [f"Recall@{k}" for k in args.recall_topk]
+    if args.include_precision:
+        headers.append("Precision")
     print(
         tabulate.tabulate(
             [[config.get_name()] + data[config] for config in exp_configs],
-            headers=["Recall@1", "Recall@5", "Recall@10", "Precision"],
+            headers=headers,
             tablefmt=args.table_format,
             floatfmt=".3f",
         )
